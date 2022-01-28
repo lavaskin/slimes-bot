2ae9a8d8d5ffb9ea1cc2fc2f4c681380ee8a5e5d
Jackson E <lavaskin@gmail.com>
2022-01-27 23:58:28 -0800

    Re enabled bot error catching (whoops!)

--- bot.py
+++ bot.py
@@ -8,16 +8,16 @@ activity = discord.Activity(type=discord.ActivityType.listening, name="s!help")
 bot = commands.Bot(command_prefix='s!', activity=activity, case_insensitive=True)
 
 
-# @bot.event
-# async def on_command_error(ctx, error):
-# 	if isinstance(error, commands.CommandOnCooldown):
-# 		# Check if more than 2 minutes remaining
-# 		if error.retry_after < 121:
-# 			await ctx.reply('You can use this command again in *{0} seconds*.'.format(int(error.retry_after)), delete_after=5)
-# 		else:
-# 			await ctx.reply('You can use this command again in *{0} minutes*.'.format(int(error.retry_after / 60)), delete_after=5)
-# 	elif isinstance(error, commands.CommandNotFound):
-# 		await ctx.reply('That command doesn\'t exist!')
+@bot.event
+async def on_command_error(ctx, error):
+	if isinstance(error, commands.CommandOnCooldown):
+		# Check if more than 2 minutes remaining
+		if error.retry_after < 121:
+			await ctx.reply('You can use this command again in *{0} seconds*.'.format(int(error.retry_after)), delete_after=5)
+		else:
+			await ctx.reply('You can use this command again in *{0} minutes*.'.format(int(error.retry_after / 60)), delete_after=5)
+	elif isinstance(error, commands.CommandNotFound):
+		await ctx.reply('That command doesn\'t exist!')
 
 @bot.event
 async def on_ready():

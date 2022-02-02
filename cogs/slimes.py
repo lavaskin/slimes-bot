ec780e1d8f94c7b0a20d961464ed180e15539c85
Jackson E <lavaskin@gmail.com>
2022-02-01 21:32:09 -0800

    Fixed slime images uploading as text files, not images

--- cogs/slimes.py
+++ cogs/slimes.py
@@ -58,6 +58,7 @@ class Slimes(commands.Cog):
 		random.seed()
 		print(' > Finished initial setup.')
 
+
 	#####################
 	# Utility Functions #
 	#####################
@@ -254,7 +255,7 @@ class Slimes(commands.Cog):
 		bucket = storage.bucket()
 		bucketPath = 'dev/' if dev else 'prod/'
 		blob = bucket.blob(f'{bucketPath}{id}.png')
-		blob.upload_from_string(path)
+		blob.upload_from_filename(path)
 
 		# Make embed and send it
 		file = discord.File(path)
